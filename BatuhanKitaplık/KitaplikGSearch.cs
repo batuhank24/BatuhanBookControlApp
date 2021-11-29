515aeb0496026b4c0b695a215ae1c64d0010875e
Batuhan K <batuhankara1721@gmail.com>
2021-11-29 11:53:11 +0300

    Aktarma
    Proje GitHub'a aktarıldı, herhangi bir güncelleme yapılmadı.

---
+++ BatuhanKitaplık/KitaplikGSearch.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.Internals;
+
+
+
+namespace BatuhanKitaplık
+{
+    public partial class KitaplikGSearch : Form
+    {
+        public KitaplikGSearch()
+        {
+            InitializeComponent();
+        }
+
+        public CefSharp.WinForms.ChromiumWebBrowser browser;
+        
+        private void BatuhanKitaplikGSearch_Load(object sender, System.EventArgs e)
+        {
+            browser = new CefSharp.WinForms.ChromiumWebBrowser("https://www.google.com")
+            {
+                Dock = DockStyle.Fill,
+                Size = new Size(600, 600),
+                Location = new Point(200,200)
+                
+            };
+            this.Controls.Add(browser);
+        }
+    }
+}
---
+++ BookApp.Models/Book.cs
@@ -0,0 +1,35 @@
+﻿namespace BookApp.Models
+{
+    public class Book
+    {
+        public Book()
+        {
+
+        }
+
+        public Book(string kitapadi, string kitapyazari, string basimevi, int basimyili, float fiyati)
+        {
+            KitapAdi = kitapadi;
+            KitapYazari = kitapyazari;
+            Basimevi = basimevi;
+            BasimYili = basimyili;
+            Fiyati = fiyati;
+        }
+        public Book(int kitapid, string kitapadi, string kitapyazari, string basimevi, int basimyili, float fiyati)
+        {
+            KitapId = kitapid;
+            KitapAdi = kitapadi;
+            KitapYazari = kitapyazari;
+            Basimevi = basimevi;
+            BasimYili = basimyili;
+            Fiyati = fiyati;
+        }
+
+        public int KitapId { get; set; }
+        public string KitapAdi { get; set; }
+        public string KitapYazari { get; set; }
+        public string Basimevi { get; set; }
+        public int BasimYili { get; set; }
+        public float Fiyati { get; set; }
+    }
+}
